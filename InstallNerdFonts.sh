0a2bacc41cc490317a61ff04a2498e2869c5d054
mabub03 <55152018+mabub03@users.noreply.github.com>
2021-06-16 21:40:07 -0400

    Added Install Nerd Fonts Script

---
+++ InstallNerdFonts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p ~/.local/share/fonts                      
+cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf
+
