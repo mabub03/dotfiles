1722f439384b1b0e84ff02f20f72ec1bec79f4ee
mabub03 <mabub03@gmail.com>
2021-06-17 20:30:13 -0400

    altered zshrc and zsh setup script

--- zsh_plugin_setup.sh
+++ zsh_plugin_setup.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
+#!/bin/zsh
+cp .zshrc $HOME
+cd $HOME
+source $HOME/.zshrc
+
+git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
+#sh - c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 git clone https://github.com/cdimascio/lambda-zsh-theme.git
 cd lambda-zsh-theme 
-cp cdimascio-lambda.zsh-theme $ZSH_CUSTOM/themes
-cd
+sudo cp cdimascio-lambda.zsh-theme $ZSH_CUSTOM/themes
+cd $HOME
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # End Script by Sourcing Zsh
-source ~/.zshrc
+source $HOME/.zshrc
