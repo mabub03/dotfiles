3753d33409c1a64886fce545d18835044b4d47f1
mabub03 <mabub03@gmail.com>
2021-06-19 20:48:43 -0400

    fixed lua lsp and added languag server install to lsp script

--- .config/nvim/lua/lsp/lua-lsp.lua
+++ .config/nvim/lua/lsp/lua-lsp.lua
@@ -1,44 +1,29 @@
--- Install by: --
--- git clone https://github.com/sumneko/lua-language-server
--- cd lua-language-server
--- git submodule update --init --recursive
+USER = vim.fn.expand('$USER')
+local sumneko_root_path = ""
+local sumneko_binary = ""
+local system_name = ""
 
--- cd 3rd/luamake
--- ninja -f ninja/linux.ninja
--- cd ../..
--- ./3rd/luamake/luamake rebuild
-
--- to run:
--- ./bin/Linux/lua-language-server -E ./main.lua
-------------------------------------------------------------------------
-local system_name
 if vim.fn.has("mac") == 1 then
   system_name = "macOS"
+  sumneko_root_path = "/Users/" .. USER .. "/.config/nvim/lua-language-server"
+  sumneko_binary = "/Users/" .. USER .. "/.config/nvim/lua-language-server/bin/" .. system_name .. "/lua-language-server"
 elseif vim.fn.has("unix") == 1 then
   system_name = "Linux"
+  sumneko_root_path = "/home/" .. USER .. "/.local/share/nvim/lua-language-server"
+  sumneko_binary = "/home/" .. USER .. "/.local/share/nvim/lua-language-server/bin/" .. system_name .. "/lua-language-server"
 else
   print("Unsupported system for sumneko")
 end
 
---USER = vim.fn.expand('$USER')
-
---local sumneko_root_path = vim.fn.stdpath('cache')..'/lspconfig/sumneko_lua/lua-language-server'
---local sumneko_binary = sumneko_root_path..'/bin'..system_name..'/lua-language-server'
-
-local sumneko_root_path = '$HOME/.config/nvim/ls/lua-language-server' 
-local sumneko_binary = '$HOME/.config/nvim/ls/lua-language-server/bin/Linux/lua-language-server'
---sumneko_root_path = "/home/" .. USER .. "/.config/nvim/lua-language-server"
---sumneko_binary = "/home/" .. USER .. "/.config/nvim/lua-language-server/bin/Linux/lua-language-server"
-
 require'lspconfig'.sumneko_lua.setup {
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  cmd = {sumneko_binary, "-E", sumneko_root_path.."/main.lua"};
   settings = {
     Lua = {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = 'LuaJIT',
         -- Setup your lua path
-        path = vim.split(package.path, ';'),
+        path = vim.split(package.path, ";"),
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
@@ -47,7 +32,7 @@ require'lspconfig'.sumneko_lua.setup {
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true, 
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
           [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
         },
       },
--- nvim_lsp_server_install.sh
+++ nvim_lsp_server_install.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
 # REQUIREMENTS:
+# RUN AS SUDO
 # npm/node, ghcup (preferably in path from the installation, pip (python package installer), perl
 # not included = c++, go, groovy, lua
 
+# Requirements
+# Debian/Ubuntu
+# apt install python3
+# apt install python3-pip
+# pip3 install cmake-language-server
+# Arch
+pacman -S python
+pacman -S python-pip
+pacman -S nodejs
+pacman -S npm
+pacman -S ninja
+#pacman -S ccls
+pip install cmake-language-server
+
 npm i -g bash-language-server
-npm install -g vscode-css-languageserver-bin
-npm install -g vscode-html-languageserver-bin
-npm install -g vscode-json-languageserver
+npm install -g vscode-langservers-extracted
 npm i -g sql-language-server
 npm i -g pyright
 npm install -g typescript typescript-language-server
 npm install -g vim-language-server
 npm install -g intelephense
-apt install python3-pip
-pip3 install cmake-language-server
+
+# Lua Install
+cd $HOME/.local/share/nvim
+git clone https://github.com/sumneko/lua-language-server
+cd lua-language-server
+git submodule update --init --recursive
+cd 3rd/luamake
+compile/install.sh
+ninja -f ninja/linux.ninja
+cd ../..
+./3rd/luamake/luamake rebuild
+
 
 # ghcup command doesn't work but the installer lets you select if you 
 #want to install the haskell langauge server from the install menu
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+# curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 # stack ghcup install hls
