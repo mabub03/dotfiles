cc9c9f5b00e4d9d76a4fdbb6da6a337fe540a116
mabub03 <mabub03@gmail.com>
2022-01-23 15:07:22 -0500

    replaced current prompt with starship prompt

--- .zshrc
+++ .zshrc
@@ -23,14 +23,18 @@ export VOLTA_HOME=$HOME/.volta
 
 export PATH=$VOLTA_HOME/bin:$PATH
 
-export PATH="$PATH:`pwd`/flutter/bin"
+export PATH="$PATH:$HOME/flutter/bin"
 export PATH="$HOME/.cargo/bin:$PATH"
-export CHROME_EXECUTABLE="/usr/bin/microsoft-edge"
+export CHROME_EXECUTABLE="/mnt/c/Program Files (x86)/Microsoft/Edge/Application/msedge.exe"
+#export CHROME_EXECUTABLE="/usr/bin/microsoft-edge"
 
 export EDITOR="nvim"
 export VISUAL="nvim"
 
-ZSH_THEME="cdimascio-lambda"
+#ZSH_THEME="cdimascio-lambda"
 plugins=(git npm zsh-autosuggestions)
 source $ZSH_CUSTOM/oh-my-zsh.sh
 
+alias ls='lsd'
+
+eval "$(starship init zsh)"
--- zsh_plugin_setup.sh
+++ zsh_plugin_setup.sh
@@ -6,12 +6,19 @@ source $HOME/.zshrc
 git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
 #sh - c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/cdimascio/lambda-zsh-theme.git
-cd lambda-zsh-theme 
-sudo cp cdimascio-lambda.zsh-theme $ZSH_CUSTOM/themes
-cd $HOME
+#git clone https://github.com/cdimascio/lambda-zsh-theme.git
+#cd lambda-zsh-theme 
+#sudo cp cdimascio-lambda.zsh-theme $ZSH_CUSTOM/themes
+#cd $HOME
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# LSD install temporiarly put in here but should probably go in fedora script
+# and wsl script when i feel like making wsl script
+cargo install lsd
+
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+cp $HOME/dotfiles/.config/starship.toml $HOME/.config
 
 # End Script by Sourcing Zsh
 source $HOME/.zshrc
