66b8aad6aa688fdd43c9d6b856ad2c3f56d1b9ae
mabub03 <mabub03@gmail.com>
2023-04-19 23:13:04 -0400

    added kde config and added steamdeck ui script

--- setup_fedora_kde.sh
+++ setup_fedora_kde.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Remove comment if you only want split clock and not full fluent and remove the git links below
+#PLASMOIDS_DIR="$HOME/.local/share/plasma/plasmoids"
+
 # disable ptrace
 sudo cp /usr/lib/sysctl.d/10-default-yama-scope.conf /etc/sysctl.d/
 sudo sed -i 's/kernel.yama.ptrace_scope = 0/kernel.yama.ptrace_scope = 3/g' /etc/sysctl.d/10-default-yama-scope.conf
@@ -39,12 +43,12 @@ sudo firewall-cmd --permanent --remove-port=1025-65535/udp
 sudo firewall-cmd --permanent --remove-port=1025-65535/tcp
 sudo firewall-cmd --permanent --remove-service=mdns
 #sudo firewall-cmd --permanent --remove-service=ssh
-sudo firewall-cmd --permanent --remove-service=samba-client
+#sudo firewall-cmd --permanent --remove-service=samba-client
 sudo firewall-cmd --reload
 
 # edit dnf.conf to make it faster and exclude gnome tour so updating doesn't reinstall it
 # maximum of parallel downloads is 20
-sudo echo 'max_parallel_downloads=15' | sudo tee -a /etc/dnf/dnf.conf
+sudo echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
 sudo echo 'fastestmirror=True' | sudo tee -a /etc/dnf/dnf.conf
 
 # setup repos
@@ -55,60 +59,53 @@ sudo flatpak remote-modify --enable flathub
 sudo flatpak remote-modify --disable fedora
 sudo flatpak remote-modify --enable flathub-beta
 # rpm fusion free and non free (don't need in fedora 35 just when setting up after install click turn on 3rd party repos button)
-sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm 
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# enable google chrome repos
-#sudo dnf install fedora-workstation-repositories
-#sudo dnf config-manager --set-enabled google-chrome
-
-# microsoft edge beta repos
-#sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-#sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
-#sudo mv /etc/yum.repos.d/packages.microsoft.com_yumrepos_edge.repo /etc/yum.repos.d/microsoft-edge-stable.repo
-
-# brave repos
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+# brave repos NOTICE: (test out flathub and if works remove this)
+#sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+#sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
 # vscode repos
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf update -y
 
-# Do not have whitespace at the end of the of the lines here or else it won't
-# Remove packages and some of these will be reinstalled with flatpak
+# remove kgpg kcharselect plasma-emojier?????
 sudo dnf remove -y \
-  evince \
-  gnome-contacts \
-  gnome-logs \
-  gnome-maps \
-  gnome-photos \
-  totem \
-  cheese \
-  gnome-system-monitor \
-  gnome-yelp \
-  gnome-boxes \
-  sassc \
-  libsass \
-  rhythmbox \
+  dnfdragora \
+  dragon \
+  kmahjongg \
+  kmines \
+  kpat \
+  konversation \
+  gnome-abrt \
+  abrt \
+  pimdataexporter \
+  kamoso \
+  kmouth \
+  kmousetool \
+  kmag \
   libreoffice-writer \
   libreoffice-calc \
   libreoffice-impress \
   firefox \
-  abrt
+  kcalc \
+  kfind \
+  kolourpaint \
+  kwrite
 
-# Install packages 
-# Do not have whitespace at the end of the of the lines here or else it won't
-# install packages and launch them instead
+# remove akonadi
+sudo dnf remove -y \*akonadi\*
+
 sudo dnf install -y \
-  gnome-tweaks \
+  kate \
+  vlc \
   neovim \
   "@C Development Tools And Libraries" \
   "@Development Tools" \
   "@Development Libraries" \
   meson \
   clang \
-  gtk3-devel \
   util-linux-user \
   libstdc++-static \
   neofetch \
@@ -123,33 +120,25 @@ sudo dnf install -y \
   python3-pip \
   openssl \
   sassc \
-  yubikey-manager \
-  yubikey-manager-qt \
-  btop \
-  brave-browser
-#  google-chrome-stable \
-#  microsoft-edge-stable
+  btop
+#  brave-browser
 
-
-# install gnome-boxes from flathub since the one from fedora packages can't load gnome os nightly 
+# install gnome-boxes from flathub since the one from fedora packages can't load gnome os nightly
 flatpak install -y flathub \
-  org.gnome.Boxes \
   org.libreoffice.LibreOffice \
-  com.mattjakeman.ExtensionManager \
-  org.freedesktop.Piper \
   com.github.tchx84.Flatseal \
   com.spotify.Client \
-  com.github.johnfactotum.Foliate \
   org.freedesktop.Platform.ffmpeg-full \
   org.mozilla.firefox \
   org.mozilla.Thunderbird \
-  com.github.rafostar.Clapper
+  com.brave.Browser
 
 flatpak install -y flathub-beta com.discordapp.DiscordCanary
 
 # enable firefox and thunderbird to use wayland
 flatpak --user override --socket=wayland --env=MOZ_ENABLE_WAYLAND=1 --device=dri --filesystem=xdg-run/pipewire-0 org.mozilla.firefox
 flatpak --user override --socket=wayland --env=MOZ_ENABLE_WAYLAND=1 --device=all --device=dri dri org.mozilla.Thunderbird
+flatpak --user override --socket=wayland com.brave.Browser
 
 echo -n "Do you want to install Nvidia drivers? [y/N]"
 read NVIDIA_PROMPT
@@ -157,7 +146,7 @@ if [[ $NVIDIA_PROMPT == "y" || $NVIDIA_PROMPT == "Y" ]]
 then
   # install nvidia drivers
   #sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
-  echo "follow the instructions to setup nvidia with secureboot here: "
+  echo "follow the instructions to setup nvidia with secureboot here: https://blog.monosoul.dev/2022/05/17/automatically-sign-nvidia-kernel-module-in-fedora-36/"
 fi
 
 echo -n "Do you want to install Steam and other gaming utilities? [y/N]"
@@ -165,7 +154,9 @@ read GAME_PROMPT
 if [[ $GAME_PROMPT == "y" || $GAME_PROMPT == "Y" ]]
 then
   # might need to add lutris, wine, and winetricks if bottles isn't enough
-  sudo dnf install -y steam gamemode gamescope
+  # add steam to dnf install if flatpak version isn't good enough
+  sudo dnf install -y gamemode
+  flatpak install flathub com.valvesoftware.Steam
 fi
 
 echo -n "Do you use a laptop? [y/N]"
@@ -174,23 +165,20 @@ if [[ $LAPTOP_PROMPT == "y" || $LAPTOP_PROMPT == "Y" ]]
 then
   sudo dnf install -y tlp
   sudo systemctl enable tlp
-  #Enable tap to click
-  gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
-  # Enable disable touchpad while typing
-  # gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing false
 fi
 
 # remove whatever isn't needed to clean up system just in case something got missed
 flatpak remove --unused
 sudo dnf autoremove -y
 
-# enable auto TRIM
-sudo systemctl enable fstrim.timer
+#install volta for a node manager
+curl https://get.volta.sh | bash
+source .bashrc
+volta install node
 
-# Move .config items
-cp -r $HOME/dotfiles/.config/btop $HOME/.config/btop
-cp -r $HOME/dotfiles/.config/gtk-3.0 $HOME/.config/gtk-3.0
-cp -r $HOME/dotfiles/.config/gtk-4.0 $HOME/.config/gtk-4.0
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# setup neovim
+#source $HOME/dotfiles/setup_nvim.sh
 
 # install fonts and update font cache
 mkdir $HOME/.local/share/fonts
@@ -204,58 +192,39 @@ rm -rf JetBrainsMono.zip
 fc-cache -fv
 cd $HOME
 
-# install gtk 3 libadwaita theme
-git clone https://github.com/lassekongo83/adw-gtk3.git
-cd adw-gtk3
-meson build
-sudo ninja -C build install
-
-#install volta for a node manager
-curl https://get.volta.sh | bash
-source .bashrc
-volta install node
-
-# setup neovim
-source $HOME/dotfiles/setup_nvim.sh
-
-# install intellij toolbox for intellij ides
-#source $HOME/dotfiles/install_jetbrains_toolbox.sh
-
-# Enable GNOME shell extensions
-cd $HOME
-mkdir gnome-extensions-repos
-cd gnome-extensions-repos
-git clone https://github.com/aunetx/blur-my-shell
-cd blur-my-shell
-make install
-cd $HOME/gnome-extensions-repos
-git clone https://github.com/ubuntu/gnome-shell-extension-appindicator.git
-cd gnome-shell-extension-appindicator
-meson gnome-shell-extension-appindicator /tmp/g-s-appindicators-build
-ninja -C /tmp/g-s-appindicators-build install
-cd $HOME
-
-# disable background logo extension
-gnome-extensions disable background-logo@fedorahosted.org
-
 # Make Fedora fonts better
-# add a if this file doesn't exist create a symlink block here
 sudo ln -fs /usr/share/fontconfig/conf.avail/10-autohint.conf /etc/fonts/conf.d
-sudo ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/  
-sudo ln -s /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/  
-sudo ln -s /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d/  
-sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/ 
-gsettings set org.gnome.desktop.interface font-antialiasing 'rgba'
-gsettings set org.gnome.desktop.interface font-hinting 'slight'
-cp $HOME/dotfiles/.config/fontconfig/fonts.conf .config/fontconfig/fonts.conf 
+sudo ln -fs /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
+sudo ln -fs /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
+sudo ln -fs /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d/
+sudo ln -fs /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
+cp $HOME/dotfiles/.config/fontconfig/fonts.conf .config/fontconfig/fonts.conf
 # move x resources to the proper place
 # remove when x is no longer used
 cp $HOME/dotfiles/.Xresources $HOME/.Xresources
 fc-cache -Ev
 
-# setup chrome desktop parameters
-#cp /usr/share/applications/google-chrome.desktop ~/.local/share/applications/google-chrome.desktop
-#sed -i 's;/usr/bin/google-chrome-stable;/usr/bin/google-chrome-stable --enable-features=WebUIDarkMode,VaapiVideoDecoder,VaapiVideoEncoder,CanvasOopRasterization --disable-gpu-driver-workarounds --use-gl=desktop --force-dark-mode;g' ~/.local/share/applications/google-chrome.desktop
+# Move .config items
+cp -r $HOME/dotfiles/.config/btop $HOME/.config/btop
+cp -r $HOME/dotfiles/.config/gtk-3.0 $HOME/.config/gtk-3.0
+cp -r $HOME/dotfiles/.config/gtk-4.0 $HOME/.config/gtk-4.0
+cp -r $HOME/dotfiles/.config/vlc $HOME/.config/vlc
+
+# remove akonadi files
+rm -rf $HOME/.config/akonadi
+rm -rf $HOME/.local/share/akonadi
+rm -rf $HOME/.local/share/akonadi_migration_agent
+
+#[[ ! -d ${PLASMOIDS_DIR} ]] && mkdir -p ${PLASMOIDS_DIR}
+#git clone https://github.com/vinceliuice/Fluent-kde.git
+#cp -r Fluent-kde/plasma/plasmoids/org.kde.plasma.splitdigitalclock ${PLASMOIDS_DIR}
+#rm -rf Fluent-kde
+
+# maybe transfer vlc config for native scrollbars and stuff
+# maybe transfer panel just cause more laziness
+
+# enable auto TRIM
+sudo systemctl enable fstrim.timer
 
 # add wifi powersave file to deactivate wifi powersave
 sudo bash -c 'cat > /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf' <<-'EOF'
@@ -263,19 +232,7 @@ sudo bash -c 'cat > /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf' <
 wifi.powersave=2
 EOF
 
-#Randomize MAC address and restart NetworkManager
-#sudo bash -c 'cat > /etc/NetworkManager/conf.d/00-macrandomize.conf' <<-'EOF'
-#[device]
-#wifi.scan-rand-mac-address=yes
-
-#[connection]
-#wifi.cloned-mac-address=stable
-#ethernet.cloned-mac-address=stable
-#connection.stable-id=${CONNECTION}/${BOOT}
-#EOF
-
 sudo systemctl restart NetworkManager
 
 echo "Setup script has finished running"
 echo "Restart your computer now for changes to take effect"
-
