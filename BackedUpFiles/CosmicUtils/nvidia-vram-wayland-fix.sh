9883d804811ddf6ad9b574134cb5bbbfa914c420
mabub03 <mabub03@gmail.com>
2025-08-01 00:19:10 -0400

    Update nvidia-vram-wayland-fix.sh

--- BackedUpFiles/CosmicUtils/nvidia-vram-wayland-fix.sh
+++ BackedUpFiles/CosmicUtils/nvidia-vram-wayland-fix.sh
@@ -2,5 +2,5 @@
 # nvidia fix vram issues on wayland compositors like cosmic comp
 # https://github.com/YaLTeR/niri/issues/1962
 
-mkdir -p /etc/nvidia/nvidia-application-profiles-rc.d/
+sudo mkdir -p /etc/nvidia/nvidia-application-profiles-rc.d/
 sudo cp 50-limit-free-buffer-pool-in-wayland-compositors.json /etc/nvidia/nvidia-application-profiles-rc.d/
